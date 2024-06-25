0364be0b549cfe4af9b5b54bbaf676a192863a0b
glbeio <yonesko@gmail.com>
2024-06-25 13:54:30 +0300

    feat(refactor): jsonredact recursive

--- dfa.go
+++ dfa.go
@@ -1,5 +1,11 @@
 package jsonredact
 
+import (
+	"bytes"
+	"fmt"
+	"reflect"
+)
+
 type dfa map[string]dfa
 
 func newDFA(expressions ...string) dfa {
@@ -22,10 +28,16 @@ func (a dfa) next(input string) dfa {
 }
 
 func (a dfa) isInTerminalState() bool {
-	return a != nil && len(a) == 0
+	return a["terminal"] != nil
 }
 
 func merge(left, right dfa) dfa {
+	if left == nil {
+		return right
+	}
+	if right == nil {
+		return left
+	}
 	automata := dfa{}
 	for _, a := range []dfa{left, right} {
 		for k := range a {
@@ -47,9 +59,62 @@ func merge(left, right dfa) dfa {
 
 func build(expressions []string) dfa {
 	if len(expressions) == 0 {
-		return dfa{}
+		return dfa{"terminal": dfa{}}
 	}
 	a := dfa{}
+	if expressions[0] == "*" {
+		return buildRecursive(expressions)
+	}
 	a[expressions[0]] = build(expressions[1:])
 	return a
+}
+
+// buildRecursive builds dfa from recursive expression, (example *.a.b, *.a.*.b)
+func buildRecursive(expressions []string) dfa {
+	root := dfa{}
+	root["#"] = root
+	//root[expressions[1]] = dfa{}
+	a := root
+	for i := 1; i < len(expressions); i++ {
+		if expressions[i] == "*" {
+			a[expressions[i-1]] = buildRecursive(expressions[i:])
+			return root
+		}
+		next := dfa{"#": root}
+		if i > 1 {
+			next[expressions[1]] = root[expressions[1]]
+		}
+		if i == len(expressions)-1 {
+			next["terminal"] = dfa{}
+		}
+		a[expressions[i]] = next
+		a = next
+	}
+	return root
+}
+
+func (a dfa) string(been map[uintptr]bool) string {
+	buffer := bytes.Buffer{}
+	ptr := reflect.ValueOf(a).Pointer()
+	if been[ptr] {
+		return ""
+	}
+	been[ptr] = true
+	if len(a) == 0 {
+		return ""
+	}
+	buffer.WriteString(fmt.Sprintf("state(%p) ", a))
+	for k, v := range a {
+		buffer.WriteString(fmt.Sprintf("%s -> %p ", k, v))
+	}
+	buffer.WriteByte('\n')
+	for _, v := range a {
+		buffer.WriteString(v.string(been))
+	}
+	return buffer.String()
+}
+
+func (a dfa) String() string {
+	been := map[uintptr]bool{}
+	return a.string(been)
 }
