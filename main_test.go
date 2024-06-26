80ac02955357bc5505f17231631ae3f7278bb214
glbeio <yonesko@gmail.com>
2024-06-26 13:10:59 +0300

    feat(refactor): jsonredact big json

--- main_test.go
+++ main_test.go
@@ -8,18 +8,7 @@ import (
 )
 
 var (
-	bigJson = `{
-  "age": 37,
-  "children": [ "Sara", "Alex", "Jack" ],
-  "fav.movie": { "title": "Deer Hunter" },
-  "fav": { "movie": "BIG" },
-  "friends": [
-    { "age": 44, "first": "Dale", "last": "Murphy" },
-    { "age": 68, "first": "Roger", "last": "Craig" },
-    { "age": 47, "first": "Jane", "last": "Murphy" }
-  ],
-  "name": { "first": "Tom", "last": "Anderson" }
-}`
+	bigJson = `[{"id":0,"name":"Elijah","city":"Austin","age":78,"friends":[{"name":"Michelle","hobbies":["Watching Sports","Reading","Skiing & Snowboarding"]},{"name":"Robert","hobbies":["Traveling","Video Games"]}]},{"id":1,"name":"Noah","city":"Boston","age":97,"friends":[{"name":"Oliver","hobbies":["Watching Sports","Skiing & Snowboarding","Collecting"]},{"name":"Olivia","hobbies":["Running","Music","Woodworking"]},{"name":"Robert","hobbies":["Woodworking","Calligraphy","Genealogy"]},{"name":"Ava","hobbies":["Walking","Church Activities"]},{"name":"Michael","hobbies":["Music","Church Activities"]},{"name":"Michael","hobbies":["Martial Arts","Painting","Jewelry Making"]}]},{"id":2,"name":"Evy","city":"San Diego","age":48,"friends":[{"name":"Joe","hobbies":["Reading","Volunteer Work"]},{"name":"Joe","hobbies":["Genealogy","Golf"]},{"name":"Oliver","hobbies":["Collecting","Writing","Bicycling"]},{"name":"Liam","hobbies":["Church Activities","Jewelry Making"]},{"name":"Amelia","hobbies":["Calligraphy","Dancing"]}]},{"id":3,"name":"Oliver","city":"St. Louis","age":39,"friends":[{"name":"Mateo","hobbies":["Watching Sports","Gardening"]},{"name":"Nora","hobbies":["Traveling","Team Sports"]},{"name":"Ava","hobbies":["Church Activities","Running"]},{"name":"Amelia","hobbies":["Gardening","Board Games","Watching Sports"]},{"name":"Leo","hobbies":["Martial Arts","Video Games","Reading"]}]},{"id":4,"name":"Michael","city":"St. Louis","age":95,"friends":[{"name":"Mateo","hobbies":["Movie Watching","Collecting"]},{"name":"Chris","hobbies":["Housework","Bicycling","Collecting"]}]},{"id":5,"name":"Michael","city":"Portland","age":19,"friends":[{"name":"Jack","hobbies":["Painting","Television"]},{"name":"Oliver","hobbies":["Walking","Watching Sports","Movie Watching"]},{"name":"Charlotte","hobbies":["Podcasts","Jewelry Making"]},{"name":"Elijah","hobbies":["Eating Out","Painting"]}]},{"id":6,"name":"Lucas","city":"Austin","age":76,"friends":[{"name":"John","hobbies":["Genealogy","Cooking"]},{"name":"John","hobbies":["Socializing","Yoga"]}]},{"id":7,"name":"Michelle","city":"San Antonio","age":25,"friends":[{"name":"Jack","hobbies":["Music","Golf"]},{"name":"Daniel","hobbies":["Socializing","Housework","Walking"]},{"name":"Robert","hobbies":["Collecting","Walking"]},{"name":"Nora","hobbies":["Painting","Church Activities"]},{"name":"Mia","hobbies":["Running","Painting"]}]},{"id":8,"name":"Emily","city":"Austin","age":61,"friends":[{"name":"Nora","hobbies":["Bicycling","Skiing & Snowboarding","Watching Sports"]},{"name":"Ava","hobbies":["Writing","Reading","Collecting"]},{"name":"Amelia","hobbies":["Eating Out","Watching Sports"]},{"name":"Daniel","hobbies":["Skiing & Snowboarding","Martial Arts","Writing"]},{"name":"Zoey","hobbies":["Board Games","Tennis"]}]},{"id":9,"name":"Liam","city":"New Orleans","age":33,"friends":[{"name":"Chloe","hobbies":["Traveling","Bicycling","Shopping"]},{"name":"Evy","hobbies":["Eating Out","Watching Sports"]},{"name":"Grace","hobbies":["Jewelry Making","Yoga","Podcasts"]}]}]`
 )
 
 func TestRedact(t *testing.T) {
@@ -243,11 +232,10 @@ func TestConcurrent(t *testing.T) {
 /*
 goos: darwin
 goarch: arm64
-pkg: jsonredact
-Benchmark/just_copy-10         	1000000000	         0.0000032 ns/op
-Benchmark/with_matched_keys-10         	  1275627	       921.1 ns/op
-Benchmark/without_matched_keys-10      	  1825269	       657.1 ns/op
-Benchmark/recursive-10                    	  455151	      2641 ns/op
+Benchmark/just_copy-10         	1000000000	         0.0000009 ns/op
+Benchmark/without_matched_keys-10         	  285404	      3945 ns/op
+Benchmark/with_matched_keys-10            	  306650	      3731 ns/op
+Benchmark/recursive-10                    	   34078	     36759 ns/op
 */
 func Benchmark(b *testing.B) {
 	b.Run("just copy", func(b *testing.B) {
