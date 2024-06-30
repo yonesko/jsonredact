39a283fb6a088166e7fb70a3c0db9be39633b7a3
glbeio <yonesko@gmail.com>
2024-06-30 23:39:03 +0300

    feat(benchmarks): jsonredact bench.sh

--- bench.sh
+++ bench.sh
@@ -1,9 +1,9 @@
 git checkout $1
 
-go test -v -benchmem -bench . -run ^$ -count=1 > $(git rev-parse --abbrev-ref HEAD).txt
+go test -v -benchmem -bench . -run ^$ -count=10 >$(git rev-parse --abbrev-ref HEAD).txt
 
 git checkout $2
 
-go test -v -benchmem -bench . -run ^$ -count=1 > $(git rev-parse --abbrev-ref HEAD).txt
+go test -v -benchmem -bench . -run ^$ -count=10 >$(git rev-parse --abbrev-ref HEAD).txt
 
 benchstat $1.txt $2.txt
