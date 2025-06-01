9d41eb1dd0cbbee1b7c7f0829f2724c74c3367fd
glebio <yonesko@gmail.com>
2025-06-01 23:53:41 +0300

    Benchmark_walkers

--- jsonwalk_test.go
+++ jsonwalk_test.go
@@ -2,6 +2,7 @@ package jsonredact
 
 import (
 	"github.com/antlr4-go/antlr/v4"
+	"os"
 	"testing"
 )
 
@@ -29,11 +30,16 @@ func Test(t *testing.T) {
 }
 
 func Benchmark_walkers(b *testing.B) {
+	bytes, err := os.ReadFile("canada.json")
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
 	b.Run("my", func(b *testing.B) {
-		jsonWalk(deepJson, printingListener{})
+		jsonWalk(string(bytes), printingListener{})
 	})
 	b.Run("antlr", func(b *testing.B) {
-		lexer := NewJSONLexer(antlr.NewInputStream(deepJson))
+		lexer := NewJSONLexer(antlr.NewInputStream(string(bytes)))
 		stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
 		parser := NewJSONParser(stream)
 		tree := parser.Json()
